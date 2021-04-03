8607040579f75f557b435efa133aced0cbe25930
Roman Peshkov <peshkovroman@gmail.com>
2021-04-03 18:01:18 +0200

    Forgotten early-init.el

---
+++ emacs/early-init.el
@@ -0,0 +1,17 @@
+(setq gc-cons-threshold most-positive-fixnum   ;; Defer Garbage collection
+      gc-cons-percentage 1.0)
+
+(add-hook 'emacs-startup-hook
+         `(lambda ()
+            (setq gc-cons-threshold 800000
+                  gc-cons-percentage 0.1)
+            (garbage-collect)) t)
+(setq frame-inhibit-implied-resize t)
+(setq default-directory "~/")
+(setq command-line-default-directory "~/")
+
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                          ("melpa" . "https://melpa.org/packages/")))
+(setq package-quickstart t)
+
+(provide 'early-init)
