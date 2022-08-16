87295e43bc1e531ad862cb5ab550e1e1c2d34265
Roman Peshkov <peshkovroman@gmail.com>
2022-08-16 09:52:44 +0200

    Switching to lua configuration for neovim

--- vim/init.lua
+++ vim/init.lua
@@ -20,6 +20,27 @@ vim.o.grepformat = "%f:%l:%c:%m,%f:%l:%m"
 vim.o.clipboard = "unnamedplus"
 vim.o.swapfile = false
 
+vim.g.mapleader = ' '
+
+vim.api.nvim_set_keymap('n', '<Leader>ev', ':vsplit $MYVIMRC<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'gx', ':silent execute "!open " . shellescape("<cWORD>")<CR>', { noremap = true })
+
+-- Shifting left/right
+vim.api.nvim_set_keymap('v', '<', '<gv', { silent = true })
+vim.api.nvim_set_keymap('v', '>', '>gv', { silent = true })
+
+-- Yank till end of line
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true, silent = true })
+
+-- Keep the cursor in place while joining lines
+vim.api.nvim_set_keymap('n', 'J', 'mzJ`z', { noremap = true })
+
+-- Open folds when jumping through matches
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true })
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true })
+
+vim.api.nvim_set_keymap('c', '%%', 'expand("%:h")."/"', { noremap = true, expr = true })
+
 -- Add package manager
 vim.cmd [[packadd packer.nvim]]
 
