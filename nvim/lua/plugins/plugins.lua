fe57987af53520e25e50bfaaa1ba8781713404d6
Roman Peshkov <peshkovroman@gmail.com>
2025-02-09 23:35:53 +0100

    render-markdown

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    }, -- if you prefer nvim-web-devicons
+    opts = {},
+  },
+  {
     "saghen/blink.cmp",
     version = "*",
     opts = {
